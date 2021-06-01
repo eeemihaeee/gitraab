ac57cbf0daea1f2cba72d833a9097b655b841399
peretyaginilya <84011538+peretyaginilya@users.noreply.github.com>
2021-06-01 23:08:33 +0500

    Выход из системы

--- logout.php
+++ logout.php
@@ -1,3 +1,10 @@
+/**
+ *
+ * @name {String} name Выход из системы
+
+ * @author {String} author Перетягин Илья
+ *
+ */
 <?php 
 	$agent=$_SERVER['HTTP_USER_AGENT'];
 	setcookie("user", $agent, time()-3600, "/");
