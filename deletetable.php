b163942d1f140a9431ffb03ebd20f52aa234f034
peretyaginilya <84011538+peretyaginilya@users.noreply.github.com>
2021-06-01 22:40:52 +0500

    Удаление таблицы

--- deletetable.php
+++ deletetable.php
@@ -1,3 +1,12 @@
+/**
+ * This is an example header component. This component is meant to be placed at the top of the page.
+ *
+ * @name {String} name Удаление таблицы
+
+ * @author {String} author Перетягин Илья
+ *
+ */
+
 <?php 
     include "BaseVar.php";
 	//УДАЛЕНИЕ ТАБЛИЦЫ	
